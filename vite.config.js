5485ca7bbeb588251d8deb1c974fbeec949d5039
NotiCon <noticon@live.dk>
2026-07-23 14:29:54 +0200

    Added configuration to deploy the site to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
+var _a, _b;
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+var repoName = (_b = (_a = process.env.GITHUB_REPOSITORY) === null || _a === void 0 ? void 0 : _a.split('/')[1]) !== null && _b !== void 0 ? _b : 'WheelOfFortune';
+var base = process.env.GITHUB_ACTIONS ? "/".concat(repoName, "/") : '/';
 export default defineConfig({
+    base: base,
     plugins: [react()],
 });

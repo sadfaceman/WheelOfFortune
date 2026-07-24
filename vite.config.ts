87bd15153371a2f69239873adfdbdf9d2fc1603d
NotiCon <noticon@live.dk>
2026-07-24 13:54:50 +0200

    Added various configuration to allow unit test

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'WheelOfFortune';
@@ -7,4 +7,10 @@ const base = process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/';
 export default defineConfig({
   base,
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: './src/setupTests.ts',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,tsx}'],
+  },
 });
